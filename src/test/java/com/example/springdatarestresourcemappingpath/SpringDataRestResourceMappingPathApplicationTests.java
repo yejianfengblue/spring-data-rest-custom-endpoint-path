7797f1a09de674a9ffe135fd7355a3a532e2b584
Jian Feng Ye <yejianfengblue@163.com>
2020-09-24 20:43:58 +0800

    Set Spring Data REST base path

--- src/test/java/com/example/springdatarestresourcemappingpath/SpringDataRestResourceMappingPathApplicationTests.java
+++ src/test/java/com/example/springdatarestresourcemappingpath/SpringDataRestResourceMappingPathApplicationTests.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -17,6 +19,9 @@ class SpringDataRestResourceMappingPathApplicationTests {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    RepositoryRestConfiguration repositoryRestConfiguration;
+
     @Test
     void testUserGreetingEndpoint() throws Exception {
 
@@ -24,6 +29,13 @@ class SpringDataRestResourceMappingPathApplicationTests {
                 get("/users/1/greeting"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("\"Hello Alice\""));
+    }
+
+    @Test
+    void testGetBasePath() {
+
+        assertThat(repositoryRestConfiguration.getBasePath().getPath())
+                .isEqualTo("/api");
     }
 
 }
